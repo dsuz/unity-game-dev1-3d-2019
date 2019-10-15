8694ca3bc83975a0e662b7d7d348e69899c50e07
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-10-15 22:05:03 +0900

    ジャンプする時に接地判定し、接地している時のみジャンプするようにした

--- Assets/PlayerControllerRb.cs
+++ Assets/PlayerControllerRb.cs
@@ -15,6 +15,8 @@ public class PlayerControllerRb : MonoBehaviour
     [SerializeField] float m_movingSpeed = 5f;
     /// <summary>ジャンプ力</summary>
     [SerializeField] float m_jumpPower = 5f;
+    /// <summary>接地判定の際、足元からどれくらいの距離を「接地している」と判定するかの長さ</summary>
+    [SerializeField] float m_isGroundedLength = 0.2f;
     Rigidbody m_rb;
     Animator m_anim;
 
@@ -48,13 +50,28 @@ public class PlayerControllerRb : MonoBehaviour
         // Animator Controller のパラメータをセットする
         m_anim.SetFloat("Speed", m_rb.velocity.magnitude);
 
-        // ジャンプの入力を取得し、押されていたらジャンプする
-        if (Input.GetButtonDown("Jump"))
+        // ジャンプの入力を取得し、接地している時に押されていたらジャンプする
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             m_rb.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
             // Animator Controller のパラメータをセットする
             m_anim.SetTrigger("Jump");
         }
+    }
+
+    /// <summary>
+    /// 地面に接触しているか判定する
+    /// </summary>
+    /// <returns></returns>
+    bool IsGrounded()
+    {
+        // Physics.Linecast() を使って足元から線を張り、そこに何かが衝突していたら true とする
+        CapsuleCollider col = GetComponent<CapsuleCollider>();
+        Vector3 start = this.transform.position + col.center;   // start: 体の中心
+        Vector3 end = start + Vector3.down * (col.center.y + m_isGroundedLength);  // end: start から真下の地点
+        Debug.DrawLine(start, end); // 動作確認用に Scene ウィンドウ上で線を表示する
+        bool isGrounded = Physics.Linecast(start, end); // 引いたラインに何かがぶつかっていたら true とする
+        return isGrounded;
     }
 }
 
