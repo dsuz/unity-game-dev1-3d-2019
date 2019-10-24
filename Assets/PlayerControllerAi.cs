69c9714888b37d0bedc99ca7330ef879a4ccf07e
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-10-25 00:40:36 +0900

    Navmesh の使い方まで更新した

---
+++ Assets/PlayerControllerAi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;   // Navmesh Agent を使うために必要
+
+public class PlayerControllerAi : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+    NavMeshAgent m_agent;
+    Vector3 m_cachedTargetPosition;
+
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_cachedTargetPosition = m_target.position;
+        m_agent.SetDestination(m_cachedTargetPosition);
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(m_cachedTargetPosition, m_target.position) > 0.1f)
+        {
+            m_cachedTargetPosition = m_target.position;
+            m_agent.SetDestination(m_cachedTargetPosition);
+        }
+    }
+}
