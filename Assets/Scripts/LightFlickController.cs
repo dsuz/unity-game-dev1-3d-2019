0d3c4ff595f99918e10b97920465a8a27ddc5fd1
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-02-02 03:12:26 +0900

    光を揺らめかせる処理を追加した

---
+++ Assets/Scripts/LightFlickController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 光をゆらめかせるコンポーネント
+/// </summary>
+[RequireComponent(typeof(Light))]
+public class LightFlickController : MonoBehaviour
+{
+    private Light m_light;
+    float m_defaultRange;
+
+    private void Start()
+    {
+        m_light = GetComponent<Light>();
+        m_defaultRange = m_light.range;
+    }
+
+    private void Update()
+    {
+        float a = Mathf.PerlinNoise(Time.time * 0.1f, 0) * m_defaultRange;
+        float b = Mathf.PerlinNoise(Time.time * 0.9f, 0) * m_defaultRange / 2;
+        m_light.range = a + b;
+    }
+}
