301941ecd653c20342a26d6fd665bfd9d9ded5f8
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-02-04 12:29:39 +0900

    ゾンビの死体の飛び方のランダム性の複雑さを上げた

--- Assets/Scripts/DeadZombieController.cs
+++ Assets/Scripts/DeadZombieController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 /// <summary>
 /// ラグドールをランダムに吹き飛ばし、dissove するコンポーネント
+/// 全ての material にはシェーダーに DissolveEmission を指定すること
 /// </summary>
 public class DeadZombieController : MonoBehaviour
 {
-    [SerializeField] float m_forceMultiplier = 100f;
+    /// <summary>吹き飛ぶ力（スカラ量）の最大値</summary>
+    [SerializeField] float m_maxForce = 100f;
+    /// <summary>吹き飛ぶ力（スカラ量）の最小値</summary>
+    [SerializeField] float m_minForce = 50f;
+    /// <summary>dissove の具合を調整するカーブ</summary>
     [SerializeField] AnimationCurve m_dissolveCurve;
+    /// <summary>dissove のシェーダープロパティ</summary>
     [SerializeField] string m_dissolvePropertyNameOfShader = "_DissolveAmount";
     float m_timer;
 
@@ -29,13 +35,16 @@ public class DeadZombieController : MonoBehaviour
     /// </summary>
     void BlowOff()
     {
+        // 力を加える方向を決める
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Vector3 dir = this.transform.position - player.transform.position;
+        Vector3 dir = this.transform.position - player.transform.position;  // プレイヤーと反対方向
         dir.y = 0;
-        dir = dir.normalized + Vector3.up;
+        dir = dir.normalized * Random.Range(0f, 2f) + Vector3.up * Random.Range(-0.3f, 1f);
+        dir = dir.normalized;
 
+        // ランダムに部位を選び出し、力を加える
         Rigidbody[] rbArray = this.transform.GetComponentsInChildren<Rigidbody>();
-        rbArray[Random.Range(0, rbArray.Length)].AddForce(dir * m_forceMultiplier, ForceMode.Impulse);
+        rbArray[Random.Range(0, rbArray.Length)].AddForce(dir * Random.Range(m_minForce, m_maxForce), ForceMode.Impulse);
     }
 
     /// <summary>
